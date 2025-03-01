56a42fa8a9d9e96d2348ffffee23e5ec6fc9c703
anderson <tomasz.ciotucha@gmail.com>
2025-03-01 11:30:38 +0100

    Test
    - Added BossAudio event references

--- Assets/2DGamekit/Scripts/Audio/BossAudio.cs
+++ Assets/2DGamekit/Scripts/Audio/BossAudio.cs
@@ -9,10 +9,12 @@ public class BossAudio : MonoBehaviour
     [Header("FMOD Events for boss")]
     public EventReference gunnerStepEvent;
     public EventReference gunnerIdleEvent;
+    public EventReference gunnerAttackEvent;
 
     [Header("FMOD Studio Event Emmiter References")]
     public StudioEventEmitter gunnerStepEmitter;
     public StudioEventEmitter gunnerIdleEmitter;
+    public StudioEventEmitter gunnerAttackEmitter;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +22,7 @@ public class BossAudio : MonoBehaviour
         // Referencing the Events to the Studio Event Emitters
         gunnerStepEmitter.EventReference = gunnerStepEvent;
         gunnerIdleEmitter.EventReference = gunnerIdleEvent;
+        gunnerAttackEmitter.EventReference = gunnerAttackEvent;
     }
 
     // Update is called once per frame
